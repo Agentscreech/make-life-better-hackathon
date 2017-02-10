900761419c70502239422130284520a960e75bd3
Travis Walentin <travis.walentin@gmail.com>
2017-02-09 17:15:58 -0800

    inital auth implementation

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
+require('dotenv').config();
+var db = require('./models');
 var express = require('express');
 var ejsLayouts = require('express-ejs-layouts');
 var bodyParser = require('body-parser');
+var session = require('express-session');
+var passport = require('./config/ppConfig');
+var flash = require('connect-flash');
+var isLoggedIn = require('./middleware/isLoggedIn');
+var path = require('path');
 var app = express();
 
 app.set('view engine', 'ejs');
@@ -8,16 +15,44 @@ app.set('view engine', 'ejs');
 app.use(require('morgan')('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(ejsLayouts);
+app.use(express.static(path.join(__dirname + '/public/')));
+
+app.use(session({
+  secret: process.env.SESSION_SECRET || 'supersecretpassword',
+  resave: false,
+  saveUninitialized: true
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+
+app.use(function(req, res, next) {
+  res.locals.alerts = req.flash();
+  res.locals.currentUser = req.user;
+  next();
+});
 
 app.get('/', function(req, res) {
   res.render('index');
 });
 
-app.get('/profile', function(req, res) {
-  res.render('profile');
+app.get('/profile', isLoggedIn, function(req, res) {
+    db.user.find({
+      where: {
+        id: req.user.id
+      },
+      include: [db.car]
+    }).then(function(user){
+      res.render('profile',{user:user});
+    });
+
 });
 
 app.use('/auth', require('./controllers/auth'));
+// app.use('/profile', require('./controllers/profile'));
+
+
 
 var server = app.listen(process.env.PORT || 3000);
 
